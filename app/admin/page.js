7203f073d97633dc554d12e47c1420976a30d908
derli <nahofalgbadamassi@gmail.com>
2026-05-28 02:16:31 +0200

    admin page

---
+++ app/admin/page.js
@@ -0,0 +1,11 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function AdminRedirect() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/?admin=1");
+  }, []);
+  return null;
+}
