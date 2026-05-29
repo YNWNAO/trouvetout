1a4f712a81a945ce5ed39451c44c8c1b722c89b0
derli <nahofalgbadamassi@gmail.com>
2026-05-29 12:34:30 +0200

    emailjs integration

--- app/api/send-email/route.js
+++ app/api/send-email/route.js
@@ -2,22 +2,24 @@ export async function POST(request) {
   try {
     const { to, subject, html } = await request.json();
 
-    const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+    const response = await fetch("https://api.emailjs.com/api/v1.0/email/send", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "api-key": process.env.BREVO_API_KEY,
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        sender: { name: "FastBuy 229", email: "nahofalgbadamassi@gmail.com" },
-        to: [{ email: to }],
-        subject,
-        htmlContent: html,
+        service_id: "service_7cfdoej",
+        template_id: "qi0vriw",
+        user_id: "oyw_7DuNRrV7R4MXT",
+        template_params: {
+          to_email: to,
+          subject: subject,
+          message: html,
+          name: "FastBuy 229",
+        },
       }),
     });
 
     if (!response.ok) {
-      const error = await response.json();
+      const error = await response.text();
       return Response.json({ error }, { status: 400 });
     }
 
